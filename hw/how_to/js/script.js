c30cfe14dac39ee0ee50ed459786a47c5fe83c30
Cameron <cnf8@miami.edu>
2018-11-05 20:33:10 -0500

    howto

---
+++ hw/how_to/js/script.js
@@ -0,0 +1,68 @@
+//Variable to track the number of steps
+var totalsteps = 5;
+
+// Track which step should be displayed
+var currentstep = 0;
+//store the html of each slide
+var currentslide;
+
+$(function(){
+  $('.totalsteps').html(totalsteps);
+
+  //hide the steps
+  $('.steps li, .end, .controls, #prev').hide();
+
+});
+
+//click events
+
+$('#begin').on('click', function(){
+  //do something here
+  $('.welcome').hide();
+  currentstep = 1;
+  currentslide = $('#step' + currentstep);
+  $(currentslide).fadeIn(1000);
+//how long the transition lasts
+$('.controls').fadeIn(1000);
+
+$('h2').html('step' + currentstep);
+});
+
+//Click on Next
+$('#next').on('click' , function(){
+  if(currentstep != totalsteps){
+$(currentslide).hide();
+currentstep++;
+currentslide = $('#step' + currentstep);
+$(currentslide).fadeIn(1000);
+$('h2').html('step' + currentstep);
+}
+if(currentstep == totalsteps){
+  $('#next').hide();
+}
+if(currentstep != 1) {
+  $('#prev').show();
+}
+});
+//Click on Previous
+$('#prev').on('click' , function(){
+
+$(currentslide).hide();
+currentstep--;
+currentslide = $('#step' + currentstep);
+$(currentslide).fadeIn(1000);
+$('h2').html('step' + currentstep);
+
+if(currentstep != totalsteps){
+  $('#next').show();
+}
+if(currentstep == 1) {
+  $('#prev').hide();
+}
+});
+
+$('#finish').on('click', function(){
+  //hide everythign again
+  $('.steps li, .controls, #prev').hide();
+  $('.end').fadeIn(1000);
+});
